297ac4ad311998db9882b8a2b37c7f3e91e16dee
DevKike <alvaronarvaez151@gmail.com>
2024-12-03 23:52:12 -0500

    Feat: implement user authentication and token generation in UserService

--- src/infrastructure/services/user/UserService.ts
+++ src/infrastructure/services/user/UserService.ts
@@ -1,16 +1,26 @@
 import { inject, injectable } from 'inversify';
-import { ICreateUser, IUser } from '../../../domain/interfaces/user/IUser';
+import {
+  IAuthCredentials,
+  ICreateUser,
+  IUser,
+} from '../../../domain/interfaces/user/IUser';
 import { IUserService } from '../../../domain/interfaces/user/IUserService';
 import { TYPES } from '../../inversify/types/types';
 import { IUserRepository } from '../../../domain/interfaces/user/IUserRepository';
-import { hash } from '../../utils/bcrypt';
+import { compare, hash } from '../../utils/bcrypt';
 import { AlreadyExistException } from '../../../domain/exceptions/AlreadyExistsException';
+import { IJwtService } from '../../jwt/interfaces/IJwtService';
+import { IJwtPayload } from '../../jwt/interfaces/IJwtPayload';
+import { NotFoundException } from '../../../domain/exceptions/NotFoundException';
+import { InvalidCredentialsException } from '../../../domain/exceptions/InvalidCredentialsException';
 
 @injectable()
 export class UserService implements IUserService {
   constructor(
     @inject(TYPES.UserRepository)
-    private readonly _userRepository: IUserRepository
+    private readonly _userRepository: IUserRepository,
+    @inject(TYPES.AuthService)
+    private readonly _authService: IJwtService
   ) {}
   async create(userData: ICreateUser): Promise<IUser> {
     try {
@@ -26,10 +36,47 @@ export class UserService implements IUserService {
 
       return userCopy;
     } catch (error) {
-        if (error instanceof Error && (error as any).code === 'ER_DUP_ENTRY') {
-          throw new AlreadyExistException('User already exists');          
-        }
+      if (error instanceof Error && (error as any).code === 'ER_DUP_ENTRY') {
+        throw new AlreadyExistException('User already exists');
+      }
       throw error;
     }
+  }
+
+  async authenticate(credentials: IAuthCredentials): Promise<string> {
+    try {
+      const user = await this._userRepository.findByEmail(credentials.email);
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      const isValidPassword = await this.validatePassword(
+        credentials.password,
+        user.password
+      );
+
+      if (!isValidPassword) {
+        throw new InvalidCredentialsException('Email or password is incorrect');
+      }
+
+      return this.generateToken(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async validatePassword(
+    plainPassword: string,
+    encryptedPassword: string
+  ): Promise<boolean> {
+    return await compare(plainPassword, encryptedPassword);
+  }
+
+  private generateToken(payload: IJwtPayload): string {
+    return this._authService.generateToken({
+      id: payload.id,
+      email: payload.email,
+    });
   }
 }
